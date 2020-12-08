9a9d65d97f87b7263b853b44d7cb575a1512ae18
DESKTOP-GUJB72J\Tyler <Tyler.martinez89@gmail.com>
2020-12-07 23:24:06 -0500

    Horizontal camera movement

--- fps-lib/src/controllers/player.rs
+++ fps-lib/src/controllers/player.rs
@@ -1,19 +1,40 @@
 use gdnative::prelude::*;
+use gdnative::api::{InputEventMouseMotion};
 
 /// The Player controller "class"
 #[derive(NativeClass)]
-#[inherit(Node)]
-pub struct Player;
+#[inherit(KinematicBody)]
+pub struct Player {
+    #[property(default = 0.03)]
+    mouse_sensitivity: f32,
+    head: Option<Ref<Node, Shared>>
+}
 
 #[methods]
 impl Player {
     /// The "constructor" of the class.
-    fn new(_owner: &Node) -> Self {
-        Player
+    fn new(_owner: &KinematicBody) -> Self {
+        Player {
+            mouse_sensitivity: 0.03,
+            head: None
+        }
     }
 
     #[export]
-    fn _ready(&self, _owner: &Node) {
+    fn _ready(&mut self, owner: &KinematicBody) {
         godot_print!("Hello from Player Controller");
+        
+        // Save a reference to the head node
+        self.head = owner.get_node("./head");
+    }
+
+    #[export]
+    fn _input(&mut self, owner: &KinematicBody, event: Ref<InputEvent>) {
+        godot_print!("Input Fired!");
+
+        if let Some(e) = event.cast::<InputEventMouseMotion>() {
+            let mouse_event = unsafe { e.assume_safe() };
+            owner.rotate_y((-mouse_event.relative().x * self.mouse_sensitivity).to_radians().into());
+        }
     }
 }
