bf91a33b06ffe62cc9f0596c03a7033738de30a3
DESKTOP-GUJB72J\Tyler <Tyler.martinez89@gmail.com>
2020-12-13 22:11:56 -0500

    Some more controller work

--- fps-lib/src/controllers/player.rs
+++ fps-lib/src/controllers/player.rs
@@ -7,7 +7,7 @@ use gdnative::api::{InputEventMouseMotion};
 pub struct Player {
     #[property(default = 0.03)]
     mouse_sensitivity: f32,
-    head: Option<Ref<Node, Shared>>
+    head: Option<Ref<Spatial, Shared>>
 }
 
 #[methods]
@@ -23,9 +23,14 @@ impl Player {
     #[export]
     fn _ready(&mut self, owner: &KinematicBody) {
         godot_print!("Hello from Player Controller");
+
+        // Capture mouse character
+        let input = Input::godot_singleton();
+        input.set_mouse_mode(2);
         
         // Save a reference to the head node
-        self.head = owner.get_node("./head");
+        let head = unsafe { owner.get_node("./head").unwrap().assume_safe().cast::<Spatial>().unwrap().claim() };
+        self.head = Some(head);
     }
 
     #[export]
@@ -35,6 +40,9 @@ impl Player {
         if let Some(e) = event.cast::<InputEventMouseMotion>() {
             let mouse_event = unsafe { e.assume_safe() };
             owner.rotate_y((-mouse_event.relative().x * self.mouse_sensitivity).to_radians().into());
+            
+            let head = unsafe { self.head.unwrap().assume_safe() };
+            head.rotate_x((-mouse_event.relative().y * self.mouse_sensitivity).to_radians().into());
         }
     }
 }
